9739a1f7ab958fc8dc0d9b1c8b92dceb6f39f19e
VitorGulicz <vdmgulicz@gmail.com>
2025-02-14 22:04:28 -0300

    update

--- Exe06_VitorGulicz.py
+++ Exe06_VitorGulicz.py
@@ -1,3 +1,7 @@
+#Nas eleições municipais os municípios com 200 mil eleitores ou mais tem segundo turno caso o primeiro colocado não tenha mais de 50% dos votos
+#Escreva um programa que leia o nome do município, a quantidade de votos do candidato mais votado. E informe s haverá segundo turno ou não
+
+
 nomeMunicipio=str(input("Digite o nome do Municipio: "))
 populaçao=int(input("Digite o numero da população do municipio:"))
 votos=int(input("Digite o numero de votos que recebeu o candidato mais votado: "))
@@ -8,4 +12,4 @@ if populaçao>=200000 and votos>populaçaoVotos:
 elif populaçao>=200000 and votos<populaçaoVotos:
     print("Haverá segundo turno - Vitor Gulicz")
 else:
-    print("Não haverá segundo turno - Vitor Gulicz")
+    print("População menor de 200000 - Vitor Gulicz")
