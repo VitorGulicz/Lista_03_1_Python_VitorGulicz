77015c1ee046fff1e40a195bd2debf8efa8b2b3f
VitorGulicz <vdmgulicz@gmail.com>
2025-02-17 20:17:51 -0300

    update

--- Exe05_VitorGulicz.py
+++ Exe05_VitorGulicz.py
@@ -6,7 +6,7 @@ preco=float(input("Digite o preço do produto: "))
 
 if preco<=100:
     precoMargem=preco+(preco*0.45)
-    print("Preço do produto com margem bruta R$: {} - Vitor Gulicz".format(precoMargem))
+    print("Preço do produto com margem bruta de 45% R$: {} - Vitor Gulicz".format(precoMargem))
 else:
     precoMargem=preco+(preco*0.25)
-    print("Preço do produto com margem bruta R$: {} - Vitor Gulicz".format(precoMargem))
+    print("Preço do produto com margem bruta de 25% R$: {} - Vitor Gulicz".format(precoMargem))
